9a14e40aee2ac4774f35fdf616071d348cd90716
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2024-03-30 20:54:42 +0300

    REndered Categories

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 
+import { imageChecker } from '../../../../utils'
 import styles from './popularjobcard.style'
 
-const PopularJobCard = () => {
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
+    <TouchableOpacity style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}>
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={{
+            uri: imageChecker(item.strCategoryThumb) ?
+              item.strCategoryThumb
+              : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwSWV-J0zJ8aj9uH0q_dINeX3ksznWH65Yhw&usqp=CAU'
+          }}
+          resizeMode='cover'
+          style={styles.logoImage} />
+      </TouchableOpacity>
+      <Text style={styles.compnayName} numberOfLines={1}>{item.strCategory}</Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {`${item.strCategoryDescription}`}
+        </Text>
+      </View>
+
+    </TouchableOpacity>
   )
 }
 
