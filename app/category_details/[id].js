c8e3e6c9287975cc1113e401a9a4802ce514f282
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2024-04-11 19:31:47 +0300

    populated meal sub info

--- app/category_details/[id].js
+++ app/category_details/[id].js
@@ -12,11 +12,68 @@ const CategoryDetails = () => {
     const strCategory = params.id
 
     const { data, isLoading, error, refetch } = useFetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${strCategory}`)
-   console.log(data)
-    
+    const [refreshing, setRefreshing] = useState(false)
+    const onRefresh = () => { }
+  
     return (
-        <Text>category details</Text>
-    ) 
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerBackVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={icons.left}
+                            dimension="60%"
+                            handlePress={() => router.back()} />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={icons.share}
+                            dimension="60%" />
+                    ),
+                    headerTitle: `${strCategory} meals`,
+                }}
+
+            />
+
+            <>
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+                >
+                    {isLoading ? (
+                        <ActivityIndicator size="large" color={COLORS.primary} />
+                    ) : error ? (
+                        <Text>Something went wrong</Text>
+                    ) : !data.meals ? (
+                        <Text>No {strCategory} found</Text>
+                    ):(
+                        data.meals?.map((meal, index) => (
+                            <View style={{ padding: SIZES.medium, paddingBottom: 100 }} key={index + 1}>
+                                <Company
+                                    companyLogo={meal.strMealThumb}
+                                    strArea={meal.strArea}
+                                    strMeal={meal.strMeal}
+                                    strCategory={meal.strCategory}
+                                    
+                                    
+
+                                />
+                                <JobTabs
+                                    data={meal} />
+                            </View>
+
+                        ))
+
+                    )}
+
+                </ScrollView>
+            </>
+        </SafeAreaView>
+    )
+
 }
- 
+
 export default CategoryDetails
